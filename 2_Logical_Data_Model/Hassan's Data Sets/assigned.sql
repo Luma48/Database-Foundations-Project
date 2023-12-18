9af445f2d0022bfaba4dac01ab2f2e3b93e10e32
Danilo-Mutavdzic <danilo.mutavdzic@student.ucll.be>
2023-12-18 13:42:16 +0100

    removed double folder

--- 2_Logical_Data_Model/Hassan's Data Sets/assigned.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE assigned_lesson (
-    employee_id INT,
-    lesson_id INT,
-    PRIMARY KEY (employee_id, lesson_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
-    FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id)
-);
