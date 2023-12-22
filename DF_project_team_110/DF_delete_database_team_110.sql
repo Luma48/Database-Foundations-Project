ec49c4a5361e009abd19fcf96f9e815defc3973b
Daniel <daniel.jaurellmevorach@student.ucll.be>
2023-12-22 23:15:50 +0100

    delete query

--- DF_project_team_110/DF_delete_database_team_110.sql
+++ DF_project_team_110/DF_delete_database_team_110.sql
@@ -1,1 +1,34 @@
-DROP SCHEMA df_project_group_110
+DELETE 
+FROM df_project_group_110.Branch;
+
+DELETE 
+FROM df_project_group_110.BranchAdministrativePosition;
+
+DELETE 
+FROM df_project_group_110.EmailAddress;
+
+DELETE 
+FROM df_project_group_110.Employee;
+
+DELETE 
+FROM df_project_group_110.Instructor;
+
+DELETE 
+FROM df_project_group_110.Lesson;
+
+DELETE 
+FROM df_project_group_110.License;
+
+DELETE 
+FROM df_project_group_110.Practical_Class;
+
+DELETE 
+FROM df_project_group_110.Student;
+
+DELETE 
+FROM df_project_group_110.studentbooklesson;
+
+DELETE 
+FROM df_project_group_110.Theoretical_Class;
+
+DROP SCHEMA df_project_group_110;
